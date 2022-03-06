4ecc432b87a86564e69e2cba3507e5914a546c32
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-03-06 11:09:36 +0600

    timeout

---
+++ timeout.js
@@ -0,0 +1,8 @@
+function doSomething() {
+  console.log("iam coding javaScript");
+}
+
+console.log("ami sobar agey");
+console.log("ami second person");
+setTimeout(doSomething, 10000);
+console.log("ami 3 no a aschi");
